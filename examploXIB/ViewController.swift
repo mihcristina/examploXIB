3f6460c2ec170a74f6326eafb16f6c9956fd1206
Michelli <contato@michellicristina.com.br>
2021-12-21 19:22:41 -0300

    chore: configura viewcode

---
+++ examploXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  examploXIB
+//
+//  Created by Michelli Cristina de Paulo Lima on 21/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
